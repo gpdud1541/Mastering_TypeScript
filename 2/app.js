b1490ae47389cd9b2ab74b44b17300116b89259f
hyeyoung <swim070@samwooim.com>
2021-11-02 11:43:36 +0900

    html 타입스크립트 적용

---
+++ 2/app.js
@@ -0,0 +1,13 @@
+var MyClass = /** @class */ (function () {
+    function MyClass() {
+    }
+    MyClass.prototype.render = function (divId, text) {
+        var el = document.getElementById(divId);
+        el.innerText = text;
+    };
+    return MyClass;
+}());
+window.onload = function () {
+    var myClass = new MyClass();
+    myClass.render("content", "Hello World");
+};

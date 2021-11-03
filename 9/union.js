88cbe34386ef5a48f88c4b6b2b5bb2a9e77ea7ad
hyeyoung <swim070@samwooim.com>
2021-11-03 14:54:03 +0900

    공용체

---
+++ 9/union.js
@@ -0,0 +1,6 @@
+// 공용체
+var unionType;
+unionType = 1;
+console.log("unionType : " + unionType);
+unionType = "test";
+console.log("unionType : " + unionType);
---
+++ 9/union.ts
@@ -0,0 +1,8 @@
+// 공용체
+var unionType: string | number;
+
+unionType = 1;
+console.log(`unionType : ${unionType}`);
+
+unionType = "test";
+console.log(`unionType : ${unionType}`);
